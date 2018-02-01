f5d04f3cc870161634e00e76ff8971d23375e7f9
charroux <benoit.charroux@free.fr>
2018-02-01 17:41:55 +0100

    update car

--- src/main/java/com/rentingService/CarRentingController.java
+++ src/main/java/com/rentingService/CarRentingController.java
@@ -25,6 +25,15 @@ public class CarRentingController {
 		return listOfCars;
 	}
 
+	@RequestMapping(value = "/cars/{index}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public Car getCar( @PathVariable(value="index") int index){
+		Car car = listOfCars.get(index);
+		System.out.println("car=" + car);
+		return car;
+	}
+
 	@RequestMapping(value = "/cars", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.OK)
 	public void addCar(@RequestBody Car car) throws Exception{
@@ -32,4 +41,31 @@ public class CarRentingController {
 		listOfCars.add(car);
 	}
 	
+	@RequestMapping(value = "/cars", method = RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
+	public void updateCar(@RequestBody Car car) throws Exception{
+		System.out.println(car);
+		int i=0;
+		while(i<listOfCars.size() && listOfCars.get(i).getPlateNumber().equals(car.getPlateNumber())==false){
+			i++;
+		}
+		if(i<listOfCars.size()){
+			listOfCars.remove(i);
+			listOfCars.add(i, car);
+		}
+	}
+
+/*	@RequestMapping(value = "/cars", method = RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
+	public void updateCar(@RequestBody Car car, @RequestParam(value="plateNumber", required=true) String  plateNumber) throws Exception{
+		System.out.println(car);
+		listOfCars.add(car);
+	}
+	
+	@RequestMapping(value = "/cars/{plateNumber}", method = RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
+	public void updateCar1(@RequestBody Car car, @PathVariable(value="plateNumber") String  plateNumber) throws Exception{
+		System.out.println(car);
+		listOfCars.add(car);
+	}*/
 }
